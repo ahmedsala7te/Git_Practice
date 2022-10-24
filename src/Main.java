7940d62bce6d66e63125d08b05034bfd2bf95317
Ahmed Salah El Dein Mohamed Ahmed Mohamed Ibrahim <Ahmed.Salaheldein@Egypt.TE-Data.core>
2022-10-24 02:46:27 +0200

    Copy files to src

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class Main {
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+
+        System.out.println(" Please enter Operator from this : + or -");
+        char operator = input.next().charAt(0);
+        if(operator == '-') {
+            Subtraction sub = new Subtraction();
+            System.out.println("Please enter first number : ");
+            int x = input.nextInt();
+            System.out.println("please enter second nymber : ");
+            int y = input.nextInt();
+            System.out.println("Result = " + sub.sub(x, y));
+        } else if (operator == '+') {
+
+            Sum sum = new Sum();
+            System.out.println("Please enter first number : ");
+            int x = input.nextInt();
+            System.out.println("please enter second nymber : ");
+            int y = input.nextInt();
+            System.out.println("Result = "+sum.add(x,y));
+
+        }
+
+
+
+
     }
 }
